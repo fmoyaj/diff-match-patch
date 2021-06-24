08075f7e15dd39eadc0fd98ad0ae3b9caae1f6c2
fmoyaj <60526175+fmoyaj@users.noreply.github.com>
2021-06-23 22:10:45 -0400

    Changed to look for whitespaces instead of lines

--- cpp/diff_match_patch.cpp
+++ cpp/diff_match_patch.cpp
@@ -531,7 +531,7 @@ QString diff_match_patch::diff_linesToCharsMunge(const QString &text,
   // text.split('\n') would would temporarily double our memory footprint.
   // Modifying text would create many large strings to garbage collect.
   while (lineEnd < text.length() - 1) {
-    lineEnd = text.indexOf('\n', lineStart);
+    lineEnd = text.indexOf(' ', lineStart);
     if (lineEnd == -1) {
       lineEnd = text.length() - 1;
     }
